23cd6d7c445d814a211544d707eeff60d6770870
NaijaTechBro <bakisodiq@gmail.com>
2023-05-07 10:18:21 +0100

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,21 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
-const Hero = () => {
-  return (
-    <div className="hero">
-      <motion.h1
-        initial={{ y: -10, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-      >
-        {/* Meet Genuine Friends with <span>QuickMeet</span> */}
-        Meet New Friends with <span>QuickMeet</span>
-      </motion.h1>
-      <p>Dating, Hanging Out and Meeting Up just got a little better and fun.</p>
-      <center>
-        <a href="/">Download App</a>
-      </center>
-    </div>
-  );
-};
+// const Hero = () => {
+//   return (
+//     <div className="hero">
+//       <motion.h1
+//         initial={{ y: -10, opacity: 0 }}
+//         whileInView={{ y: 0, opacity: 1 }}
+//       >
+//         {/* Meet Genuine Friends with <span>QuickMeet</span> */}
+//         Meet New Friends with <span>QuickMeet</span>
+//       </motion.h1>
+//       <p>Dating, Hanging Out and Meeting Up just got a little better and fun.</p>
+//       <center>
+//         <a href="/">Download App</a>
+//       </center>
+//     </div>
+//   );
+// };
 
-export default Hero;
+// export default Hero;
